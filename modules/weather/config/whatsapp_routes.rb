2f7772c5fe5f649ea2d0efc39fcdc8bcd846e5ff
Jon de Andres <jon@wuaki.tv>
2014-08-20 23:54:56 +0200

    Fix current weather prefix.

--- modules/weather/config/whatsapp_routes.rb
+++ modules/weather/config/whatsapp_routes.rb
@@ -1,3 +1,3 @@
 Weather::Engine.whatsapp_routes.draw do
-  text '(?<place>.*)', to: 'weather/main#current', prefix: 'temp|Temp'
+  text '(?<place>.*)', to: 'weather/main#current', prefix: 'Temp'
 end
