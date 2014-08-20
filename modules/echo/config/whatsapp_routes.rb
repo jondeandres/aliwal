485463d70bbf5fcf172df8f7c438fb6c79c33167
Jon de Andres <jon@wuaki.tv>
2014-08-20 23:55:21 +0200

    Change echo prefixes => Camlize them.

--- modules/echo/config/whatsapp_routes.rb
+++ modules/echo/config/whatsapp_routes.rb
@@ -1,4 +1,4 @@
 Echo::Engine.whatsapp_routes.draw do
-  text '(?<text>.*)', to: 'echo/echo#text', prefix: 'echo'
-  text '(?<text>.*)', to: 'echo/echo#upcase', prefix: 'up'
+  text '(?<text>.*)', to: 'echo/echo#text', prefix: 'Echo'
+  text '(?<text>.*)', to: 'echo/echo#upcase', prefix: 'Up'
 end
