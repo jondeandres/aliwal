5217287ccbc1f66cadf2dd4e25498cf604c8c69a
igayoso <igayoso@gmail.com>
2014-08-21 02:50:48 +0200

    Add tomorrow weather

--- modules/weather/app/handlers/weather/main_handler.rb
+++ modules/weather/app/handlers/weather/main_handler.rb
@@ -4,9 +4,12 @@ require 'weather/request'
 module Weather
   class MainHandler < Aliwal::Handler::Base
     def current
-      send_text current_weather_text(current_weather)
+      send_text current_weather_text(weather)
     end
 
+    def tomorrow 
+      send_text tomorrow_weather_text(weather)
+    end
     private
 
     def current_weather_text(data)
@@ -22,12 +25,26 @@ Humidity: #{current_data['humidity']}
 END
     end
 
+    def tomorrow_weather_text(data)
+      not_found_place(data['error'][0]) if data['error']
+      tomorrow_data = data['weather'][0]
+      request_data = data['request'][0]
+
+      <<-END
+Location: #{request_data['query']}, #{request_data['type']}
+Description: #{tomorrow_data['weatherDesc'][0]['value']}
+Temperature: #{tomorrow_data['tempMinC']}/#{tomorrow_data['tempMaxC']}
+Humidity: #{tomorrow_data['humidity']}
+Precip: #{tomorrow_data['precipMM']}
+END
+    end
+
     def not_found_place(error)
       send_text error['msg']
       return
     end
 
-    def current_weather
+    def weather
       Weather::Request.request(params[:place])
     end
   end
--- modules/weather/config/whatsapp_routes.rb
+++ modules/weather/config/whatsapp_routes.rb
@@ -1,3 +1,4 @@
 Weather::Engine.whatsapp_routes.draw do
   text '(?<place>.*)', to: 'weather/main#current', prefix: 'Temp'
+  text '(?<place>.*)', to: 'weather/main#tomorrow', prefix: 'Temptomorrow'
 end
