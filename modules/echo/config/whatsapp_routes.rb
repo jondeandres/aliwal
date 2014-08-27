d9094479a111635ffa9f60606dfe6b46eb6645da
Jon de Andres <jon@wuaki.tv>
2014-08-27 12:09:03 +0200

    Change Magia prefix for memes to Meme.

--- modules/echo/config/whatsapp_routes.rb
+++ modules/echo/config/whatsapp_routes.rb
@@ -1,5 +1,5 @@
 Echo::Engine.whatsapp_routes.draw do
-  text 'Magia (?<top>.*)', to: 'echo/echo#meme'
+  text 'Meme (?<top>.*)', to: 'echo/echo#meme'
   text '(?<text>.*)', to: 'echo/echo#text', prefix: 'Echo'
   text '(?<text>.*)', to: 'echo/echo#upcase', prefix: 'Up'
   text '(?<text>.*)', to: 'echo/echo#reverse', prefix: 'Reverse'
